c7bb773f4252338902bbf5dbfc9ddb31263f6361
Karol Szymański <karol2134@op.pl>
2023-07-07 11:21:34 +0200

    First Commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+const Container = styled.View`
+  flex: 1;
+  background-color: #c61d1d;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Container>
+    <View>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
